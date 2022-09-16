aef8761bd6fb4da57c81aa013b75bb6a44c5cc92
Mauro <MauroMarchesan88@gmail.com>
2022-09-16 10:31:23 -0300

    Req18 completo

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const BikeZodSchema = z.object({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number({
+    required_error: 'engineCapacity is required',
+    invalid_type_error: 'engineCapacity must be a number',
+  }).gt(0, { message: 'engineCapacity must be 0 or higher' })
+    .lte(2500, { message: 'engineCapacity must be lower than 7' })
+    .int(),
+});
+
+const BikeWtihVehicleSchema = z.intersection(VehicleZodSchema, BikeZodSchema);
+
+type IMotorcycle = z.infer<typeof BikeWtihVehicleSchema>;
+
+export { BikeWtihVehicleSchema, IMotorcycle };
